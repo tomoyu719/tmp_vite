ce0f33a6d54536e5e6340a4caab5b50788bea9a6
tomoyu719 <tomoyuki719245@gmail.com>
2024-11-06 17:55:22 +0900

    feat: add code coverage configuration to Vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,10 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+    },
+    include: ["tests/unit/**/*.{test,spec}.tsx"],
   },
 });
